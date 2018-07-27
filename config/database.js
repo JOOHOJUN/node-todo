e7a63b4c6772bdb57b8bb4d09f64411ab5174203
JOOHOJUN <joo4456@naver.com>
2018-07-27 17:19:11 +0900

    웹앱 배포

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
     remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
-    localUrl: 'mongodb://localhost/meanstacktutorials'
+    localUrl: 'mongodb://pratice:S7CMQjdbCGYIeQKBx90dQgLh62k8cqbx7uCXcyqLAsDLDtGpysSYCv18D8EnmbHawH8fNVuqQvEWRkg93liAew==@pratice.documents.azure.com:10255/?ssl=true&replicaSet=globaldb'
 };
